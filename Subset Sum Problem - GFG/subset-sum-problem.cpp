9af3486b922594385acc33a69cc70019abb90e73
RANADIP MUNDA <91272088+ranadip01in@users.noreply.github.com>
2023-02-27 01:15:48 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,23 +9,23 @@ using namespace std;
 
 class Solution{   
 public:
-//DP + Memoization
+//DP + Tabulation
 //TC - O(n*k)
 //SC - O(n*k)
-    bool checkSubArray(int idx,int k,vector<int>& nums,vector<vector<int>> &dp){
-        if(k==0) return true;
-        if(idx==0) return nums[0]==k;
-        if(dp[idx][k]!=-1) return dp[idx][k];
-        bool nonTake = checkSubArray(idx-1,k,nums,dp);
-        bool take = false;
-        if(k>=nums[idx] && nums[idx]!=0) take = checkSubArray(idx-1,k-nums[idx],nums,dp);
-        dp[idx][k] = take or nonTake;
-        return dp[idx][k];
-    }
     bool isSubsetSum(vector<int>nums, int sum){
         int n = nums.size();
-        vector<vector<int>> dp(102,vector<int>(1e5+2,-1));
-        return checkSubArray(n-1,sum,nums,dp);
+        vector<vector<bool>> dp(n,vector<bool>(sum+1,false));
+        for(int i=0;i<n;++i) dp[i][0] = true;
+        dp[0][nums[0]] = true;
+        for(int i=1;i<n;++i){
+            for(int j=1;j<=sum;++j){
+                bool nonTake = dp[i-1][j];
+                bool take = false;
+                if(j>=nums[i]) take = dp[i-1][j-nums[i]];
+                dp[i][j] = take or nonTake;
+            }
+        }
+        return dp[n-1][sum];
         
     }
 };
