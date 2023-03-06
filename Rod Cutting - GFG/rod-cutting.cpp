c61e2d2583846ae1928e5d8274fbc1d3de0c8007
RANADIP MUNDA <91272088+ranadip01in@users.noreply.github.com>
2023-03-07 01:13:57 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,22 +10,21 @@ using namespace std;
 
 class Solution{
   public:
-//DP + Memoization
+//DP + Tabulation
 //TC - O(n*n)
 //SC - O(n*n)
-    int maxProfit(int idx,int n,int price[],vector<vector<int>> &dp){
-        if(n==0) return 0;
-        if(idx==0) return n*price[0];
-        if(dp[idx][n]!=-1) return dp[idx][n];
-        int notTake = maxProfit(idx-1,n,price,dp);
-        int take=0;
-        if(idx+1<=n) take = price[idx] + maxProfit(idx,n-idx-1,price,dp);
-        dp[idx][n] = max(take,notTake);
-        return dp[idx][n];
-    }
     int cutRod(int price[], int n) {
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return maxProfit(n-1,n,price,dp);
+        vector<vector<int>> dp(n,vector<int>(n+1,0));
+        for(int i=1;i<=n;++i) dp[0][i]=i*price[0];
+        for(int i=1;i<n;++i){
+            for(int j=1;j<=n;++j){
+                int notTake = dp[i-1][j];
+                int take=0;
+                if(i+1<=j) take = price[i] + dp[i][j-i-1];
+                dp[i][j] = max(take,notTake);
+            }
+        }
+        return dp[n-1][n];
     }
 };
 
