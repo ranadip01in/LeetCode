05638ab9189513885eef13ef21cc4b0c29df3840
RANADIP MUNDA <91272088+ranadip01in@users.noreply.github.com>
2023-04-20 09:11:14 +0530

    Added solution - LeetHub

---
+++ Word Ladder I - GFG/word-ladder-i.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int wordLadderLength(string beginWord, string endWord, vector<string>& wordList) {
+        set<string> st(wordList.begin(),wordList.end());
+        for(auto it:wordList) st.insert(it);
+        st.erase(beginWord);
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        while(!q.empty()){
+            string word=q.front().first;
+            int path=q.front().second;
+            q.pop();
+            if(word==endWord) return path;
+            for(int i=0;i<word.length();++i){
+                char original=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.count(word)){
+                        st.erase(word);
+                        q.push({word,path+1});
+                    }
+                }
+                word[i]=original;
+            }
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<string>wordList(n);
+		for(int i = 0; i < n; i++)cin >> wordList[i];
+		string startWord, targetWord;
+		cin >> startWord >> targetWord;
+		Solution obj;
+		int ans = obj.wordLadderLength(startWord, targetWord, wordList);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
