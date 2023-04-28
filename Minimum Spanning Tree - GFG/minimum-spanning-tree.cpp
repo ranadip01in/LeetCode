8ddd420cc15e393270df897aba772f4d2249c59d
RANADIP MUNDA <91272088+ranadip01in@users.noreply.github.com>
2023-04-28 10:59:20 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,33 +3,78 @@
 using namespace std;
 
 // } Driver Code Ends
+
+
+class DSU{
+    vector<int> rank,parents,Size;
+public:
+    DSU(int n){
+        rank.resize(n+1,0);
+        parents.resize(n+1,0);
+        Size.resize(n+1,1);
+        for(int i=0;i<=n;++i) parents[i]=i;
+    }
+    int findUPar(int n){
+        if(n==parents[n]) return n;
+        return parents[n]=findUPar(parents[n]);
+    }
+    void unionByRank(int u,int v){
+        int ulp_v=parents[v];
+        int ulp_u=parents[u];
+        if(ulp_v==ulp_u) return;
+        if(rank[ulp_u]<rank[ulp_v]) parents[ulp_u]=ulp_v;
+        else if(rank[ulp_v]<rank[ulp_u]) parents[ulp_v]=parents[ulp_u];
+        else{
+            parents[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u,int v){
+        int ulp_v=parents[v];
+        int ulp_u=parents[u];
+        if(ulp_u==ulp_v) return;
+        if(Size[ulp_u]<Size[ulp_v]){
+            parents[ulp_u]=ulp_v;
+            Size[ulp_v]+=Size[ulp_u];
+        }else{
+            parents[ulp_v]=ulp_u;
+            Size[ulp_u]+=Size[ulp_v];
+        }
+    }
+};
 class Solution
 {
-public:
-//TC - O(ElogV)
-//SC - O(V)
+//TC - O(M*4*alpha*2)
+//SC - O(M)
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        vector<int> vis(V,0);
-        set<pair<int,int>> st;
-        // {wt,node}
-        st.insert({0,0});
-        int sum=0;
-        while(!st.empty()){
-            auto it = *(st.begin());
-            st.erase(it);
-            int node = it.second;
-            int wt = it.first;
-            if(vis[node]) continue;
-            vis[node]=1;
-            sum+=wt;
-            for(auto iter:adj[node]){
-                int adjNode = iter[0];
-                int adjWt = iter[1];
-                if(!vis[adjNode]) st.insert({adjWt,adjNode});
+        vector<pair<int,pair<int,int>>> edges;
+        //TC - O(V+E)
+        for(int i=0;i<V;++i){
+            for(auto it:adj[i]){
+                int adjNode=it[0];
+                int wt=it[1];
+                int node=i;
+                edges.push_back({wt,{node,adjNode}});
             }
         }
-        return sum;
+        //TC - O(MlogM)
+        sort(edges.begin(),edges.end());
+        DSU ds(V);
+        int mstWt=0;
+        //TC - O(M*4*alpha)
+        for(auto it:edges){
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            if(ds.findUPar(u)!=ds.findUPar(v)){
+                mstWt+=wt;
+                ds.unionByRank(u,v);
+            }
+        }
+        return mstWt;
     }
 };
 
